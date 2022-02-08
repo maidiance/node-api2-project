8af26546f441823c784757fb3d0463c9d21837af
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 17:06:17 -0500

    debugging complete

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const postsRouter = require('./posts/posts-router');
 
 const server = express();
+server.use(express.json());
 server.use('/api/posts', postsRouter);
 
 module.exports = server;
