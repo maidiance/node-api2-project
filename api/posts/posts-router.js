239efeeb10188c81ee6941122214ae547f7abadb
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 16:42:22 -0500

    additional refactoring for npm tests in progress

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -47,25 +47,25 @@ router.post('/', (req, res) => {
     }
 });
 
-router.put('/:id', async(req, res) => {
+router.put('/:id', (req, res) => {
     const { id } = req.params;
-    try{
-        const post = await post.findById(id);
-        if(post == null){
-            res.status(404).json({message: 'The post with the specified ID does not exist'});
-            return;
-        }
-        const body = req.body;
-        if(!body.title || !body.contents) {
-            res.status(400).json({message: 'Please provide title and contents for the post'});
-            return;
-        } else {
-            const newPost = await post.update(id, body);
-            res.status(200).json(newPost);
-        }
-    } catch (e) {
-        res.status(500).json({message: 'The post information could not be modified'});
-    }
+    const body = req.body;
+    post.update(id, body)
+        .then(post => {
+            if(post == null) {
+                res.status(404).json({message: 'The post with the specified ID does not exist'});
+            } else if (!post.title || !post.contents) {
+                res.status(400).json({message: 'Please provide title and contents for the post'});
+            } else {
+                res.status(200).json({
+                    id: post.id,
+                    ...body,
+                });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({message: 'The post information could not be modified'});
+        })
 });
 
 router.delete('/:id', (req, res) => {
