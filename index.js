f1196713c9de5cce84dd8d3dc3e433bc4c3ec3b9
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 14:02:13 -0500

    index.js set up

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server.js');
+
+const port = 8080;
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port} \n`);
+})
