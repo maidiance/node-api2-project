3c605f01d0aba2a18bc8c2aaa68d465d354f8746
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 14:19:47 -0500

    put post endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,11 @@
 // implement your posts router here
 const { Router } = require('express');
-const Post = require('./posts-model');
+const post = require('./posts-model');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    Post.find()
+    post.find()
         .then(posts => {
             res.status(200).json(posts);
         })
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    Post.findById(id)
+    post.findById(id)
         .then(post => {
             if(post == null) {
                 res.status(404).json({message: 'The post with the specified ID does not exist'});
@@ -27,4 +27,19 @@ router.get('/:id', (req, res) => {
         .catch(() => {
             res.status(500).json({message: 'The post information could not be retrieved'});
         })
+});
+
+router.post('/', (req, res) => {
+    const post = req.body;
+    if(!post.title || !post.contents){
+        res.status(400).json({message: 'Please provide title and contents for the post'});
+    } else {
+        post.insert(post)
+            .then(post => {
+                res.status(201).json(post);
+            })
+            .catch(() => {
+                res.status(500).json({message: 'There was an error while saving the post to the database'});
+            })
+    }
 });
