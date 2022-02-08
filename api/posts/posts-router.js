ad52be661929a22fb96d883a8fa17d33d2c2ba86
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 14:14:33 -0500

    get post by id endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,15 +1,30 @@
 // implement your posts router here
 const { Router } = require('express');
-const User = require('./posts-model');
+const Post = require('./posts-model');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    User.find()
-        .then(users => {
-            res.status(200).json(users);
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
         })
         .catch(() => {
-            res.status(500).json({message: 'The posts information could not be retrieved'})
+            res.status(500).json({message: 'The posts information could not be retrieved'});
         })
-})
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            if(post == null) {
+                res.status(404).json({message: 'The post with the specified ID does not exist'});
+            } else {
+                res.json(post);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({message: 'The post information could not be retrieved'});
+        })
+});
