4c37212e4beab45b2d4ca3094b3541ad92ef7a8e
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 14:09:36 -0500

    get all posts endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+const { Router } = require('express');
+const User = require('./posts-model');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(() => {
+            res.status(500).json({message: 'The posts information could not be retrieved'})
+        })
+})
