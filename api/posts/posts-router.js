91e7e61b145bf0d542f5cf3feaefe2e17ebbf0d2
Winifred Lu <winnie.lu@comcast.net>
2022-02-08 14:27:01 -0500

    put post endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -43,3 +43,24 @@ router.post('/', (req, res) => {
             })
     }
 });
+
+router.put('/:id', async(req, res) => {
+    const { id } = req.params;
+    try{
+        const post = await post.findById(id);
+        if(post == null){
+            res.status(404).json({message: 'The post with the specified ID does not exist'});
+            return;
+        }
+        const body = req.body;
+        if(!body.title || !body.contents) {
+            res.status(400).json({message: 'Please provide title and contents for the post'});
+            return;
+        } else {
+            const newPost = await post.update(id, body);
+            res.status(200).json(newPost);
+        }
+    } catch (e) {
+        res.status(500).json({message: 'The post information could not be modified'});
+    }
+});
